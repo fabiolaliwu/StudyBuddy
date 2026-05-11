011497aaa211ee8323579de9fd670644527ea5b7
Fabiola Li Wu <fabiliwu@hotmail.com>
2026-05-10 20:47:13 -0400

    updated reasearch notes

--- StudyBuddy/src/pages/Research/Research.jsx
+++ StudyBuddy/src/pages/Research/Research.jsx
@@ -12,11 +12,29 @@ function Research() {
         return (
           <div className="tab-pane">
             <div className="ai-chat-placeholder">
-                <h4>Set up</h4>
+                <p className="ai-placeholder-text">Manual cleaning: We can use software like Blender to clean data manually by deleting unnecessary keyframes, but this is very time consuming.</p>
+                <p className="ai-placeholder-text">Neural network/AI approach: not urgent but good to have</p>
+                <p className="ai-placeholder-text">Inverse Kinematics approach: focused on the relationship between joints</p>
+
                 <br></br>
+                <br></br>
+                <h4>Set up</h4>
                 <p className="ai-placeholder-text">- Download Unity Hub</p>
                 <p className="ai-placeholder-text">- Download Blender</p>
                 <p className="ai-placeholder-text">- Download Github Desktop</p>
+
+                <br></br>
+                <br></br>
+                <h4>Resources</h4>
+                <p className="ai-placeholder-text">
+                    <a href="https://www.youtube.com/watch?v=xD7qUfSOGOE" target="_blank" rel="noopener noreferrer">Japanese Tea Ceremony</a>
+                </p>
+                <p className="ai-placeholder-text">
+                    <a href="https://www.youtube.com/watch?v=4wQVxj_0Mdo" target="_blank" rel="noopener noreferrer">Hands motions and Tea Master's posture</a>
+                </p>
+
+                
+                
 
             </div>
           </div>
