1893829b5ba904e693be54c1b1c36d5d1cab5ae6
Fabiola Li Wu <fabiliwu@hotmail.com>
2026-05-19 03:21:42 -0400

    updated format

--- StudyBuddy/src/pages/Research/Research.jsx
+++ StudyBuddy/src/pages/Research/Research.jsx
@@ -12,17 +12,17 @@ function Research() {
         return (
           <div className="tab-pane">
             <div className="ai-chat-placeholder">
-                <p className="ai-placeholder-text">Manual cleaning: We can use software like Blender to clean data manually by deleting unnecessary keyframes, but this is very time consuming.</p>
-                <p className="ai-placeholder-text">Neural network/AI approach: not urgent but good to have</p>
-                <p className="ai-placeholder-text">Inverse Kinematics approach: focused on the relationship between joints</p>
+                Manual cleaning: We can use software like Blender to clean data manually by deleting unnecessary keyframes, but this is very time consuming.
+                <br></br>Neural network/AI approach: not urgent but good to have.
+                <br></br>Inverse Kinematics approach: focused on the relationship between joints.
 
-                <br></br>
+                <br></br><br></br>
                 <h4>Set up</h4>
-                <p className="ai-placeholder-text">- Download Unity Hub</p>
-                <p className="ai-placeholder-text">- Download Blender</p>
-                <p className="ai-placeholder-text">- Download Github Desktop</p>
+                - Download Unity Hub
+                <br></br>- Download Blender
+                <br></br>- Download Github Desktop
 
-                <br></br>
+                <br></br><br></br>
                 <h4>Resources</h4>
                   <a href="https://www.youtube.com/watch?v=xD7qUfSOGOE" target="_blank" rel="noopener noreferrer">Japanese Tea Ceremony</a><br></br>
                   <a href="https://www.youtube.com/watch?v=4wQVxj_0Mdo" target="_blank" rel="noopener noreferrer">Hands motions and Tea Master's posture</a><br></br>
